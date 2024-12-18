25384354f8494aaa23d270155716b0e21ded0400
nppe6 <xiao2313667401@foxmail.com>
2024-12-18 15:22:11 +0800

    组件内的 env 进行转换

--- types/viteEnv.d.ts
+++ types/viteEnv.d.ts
@@ -3,3 +3,12 @@ interface ViteEnv {
   VITE_ROUTE_AUTOLOAD: boolean
   VITE_API_URL: string
 }
+
+interface ImportMetaEnv extends ViteEnv {
+  readonly VITE_APP_TITLE: string
+  // 更多环境变量...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
