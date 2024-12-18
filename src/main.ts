4e83b88f5b01ab7283de693518022eedb4d45439
nppe6 <xiao2313667401@foxmail.com>
2024-12-18 11:19:25 +0800

    配置plugins目录文件 已完成对tailwindcss插件基础配置

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router, { setupRouter } from '@/router'
+import { setupPlugins } from './plugins'
 
 async function bootstrap() {
   const app = createApp(App)
   setupRouter(app)
+
+  setupPlugins(app)
 
   // 等待页面路由挂载完成以后再渲染页面
   await router.isReady()
--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>login.vue</div>
+  <div class="bg-pink-300 text-red-900 text-[48px]">login.vue</div>
 </template>
 
 <style scoped></style>
