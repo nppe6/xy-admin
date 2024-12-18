6e2efcb0c4714065c1fbf43d0676ab0693c32b31
nppe6 <xiao2313667401@foxmail.com>
2024-12-18 11:49:18 +0800

    类型声明文件的定义 对env进行类型声明

---
+++ types/viteEnv.d.ts
@@ -0,0 +1,5 @@
+interface ViteEnv {
+  VITE_ROUTE_SELECT: number
+  VITE_ROUTE_AUTOLOAD: boolean
+  VITE_API_URL: string
+}
--- vite/plugins/index.ts
+++ vite/plugins/index.ts
@@ -2,7 +2,7 @@ import { Plugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { setupMockPlugins } from './mock'
 
-export function setupVitePlugins(isBuild: boolean, env: Record<string, any>) {
+export function setupVitePlugins(isBuild: boolean, env: ViteEnv) {
   const plugins: Plugin[] = [vue()]
 
   plugins.push(setupMockPlugins(isBuild))
