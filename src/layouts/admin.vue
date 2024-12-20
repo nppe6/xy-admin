fd8e929effc31ae79aa91833f0776335f9ca4f88
nppe6 <xiao2313667401@foxmail.com>
2024-12-20 16:33:13 +0800

    后台首页基础布局 模板

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts"></script>
 <template>
-  <router-view></router-view>
+  <div class="flex">
+    <div class="admin-menu">
+      <div class="admin-menu-warp xy-menu-warp-fixed">1</div>
+    </div>
+    <div class="admin-main"><router-view></router-view></div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.admin-menu {
+  @apply flex-shrink-0 w-[208px] bg-white min-h-screen;
+
+  .admin-menu-warp {
+    @apply text-gray-900;
+  }
+}
+
+.xy-menu-closed {
+  @apply w-12 overflow-hidden;
+}
+
+.admin-main {
+  @apply flex-grow bg-[#f6f6f8];
+}
+</style>
 
 <script>
 export default {
