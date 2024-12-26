b5d76a0f60bbbe4ef9b56ecbb8debb1c337bd6f7
nppe6 <xiao2313667401@foxmail.com>
2024-12-26 16:35:24 +0800

    添加错误页面

--- src/views/error/403.vue
+++ src/views/error/403.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>403页面</div>
+  <div class="relative h-screen overflow-hidden bg-indigo-900">
+    <img src="/src/assets/image/8.svg" class="absolute object-cover w-full h-full" />
+    <div class="absolute inset-0 bg-black opacity-25"></div>
+    <div class="container relative z-10 flex items-center px-6 py-32 mx-auto md:px-12 xl:py-40">
+      <div class="relative z-10 flex flex-col items-center w-full font-mono">
+        <h1 class="mt-4 text-5xl font-extrabold leading-tight text-center text-white">You&#x27;re alone here</h1>
+        <h4 class="text-2xl font-extrabold leading-tight text-center text-white">你访问的资源不存在 ~</h4>
+        <p class="font-extrabold text-white text-8xl my-44 animate-bounce">403</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
