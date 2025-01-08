998abfae29eab3b6f9f4c14052fb9685ec8c3ee7
nppe6 <xiao2313667401@foxmail.com>
2025-01-08 11:41:26 +0800

    按需加载 进行调整 并新增加 按需加载 vue的api

--- vite/plugins/autoImport.ts
+++ vite/plugins/autoImport.ts
@@ -4,10 +4,13 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import ElementPlus from 'unplugin-element-plus/vite'
 
-export function setupElementPlus() {
+export function setupAutoImport() {
   return [
     AutoImport({
       resolvers: [ElementPlusResolver()],
+      imports: ['vue', 'vue-router'],
+      // 按需加载我们使用 vue的 api 和 router api
+      dts: 'types/auto-imports.d.ts',
     }),
     // 这里的 自动按需导入 不是 值作用于 elementui 而是适用 所有的组件
     Components({
--- vite/plugins/index.ts
+++ vite/plugins/index.ts
@@ -1,7 +1,7 @@
 import { Plugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { setupMockPlugins } from './mock'
-import { setupElementPlus } from './elementui'
+import { setupAutoImport } from './autoImport'
 import { visualizer } from 'rollup-plugin-visualizer'
 
 export function setupVitePlugins(isBuild: boolean, env: ViteEnv) {
@@ -10,7 +10,7 @@ export function setupVitePlugins(isBuild: boolean, env: ViteEnv) {
   const plugins: Plugin[] = [vue()]
 
   plugins.push(setupMockPlugins(isBuild))
-  plugins.push(...setupElementPlus())
+  plugins.push(...setupAutoImport())
   plugins.push(visualizer())
 
   return plugins
